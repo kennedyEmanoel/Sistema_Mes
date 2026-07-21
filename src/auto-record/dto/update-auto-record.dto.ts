28ee0164027ba765e53fa9565e154114cae4dd82
Kennedy <kennedy1emanoel19@gmail.com>
2026-07-20 21:28:53 -0300

    remove auto-record

--- src/auto-record/dto/update-auto-record.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateAutoRecordDto } from './create-auto-record.dto';
-
-export class UpdateAutoRecordDto extends PartialType(CreateAutoRecordDto) {}
